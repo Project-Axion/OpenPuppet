418f6ac6372ff92a537d49370dfc96794fc41253
William Dawson <William@wtdawson.info>
2026-06-30 23:31:55 +0100

    Post build script and other stuff

---
+++ src/OpenPuppet/postbuild.bat
@@ -0,0 +1,18 @@
+﻿::@echo off
+
+:: This script will be run after every build
+
+set "config=%1"
+set "base=..\..\bin\%config%\net9.0"
+set "plugins=%base%\Plugins"
+set "sources=..\Plugins"
+
+:: Check if the directory exists
+if not exist "%plugins%" (
+	mkdir "%plugins%"
+)
+
+:: Copy each of the plugins
+FOR /D %%D IN ("%sources%\*") DO (
+    xcopy "%%D\bin\%config%\net8.0" "%plugins%\%%~nxD" /E /I /Y >nul
+)
