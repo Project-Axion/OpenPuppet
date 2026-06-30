3ad693193788e53cc4407ac8ba545e75cddbf285
William Dawson <William@wtdawson.info>
2026-06-30 23:48:08 +0100

    Plugin management stuff

--- src/OpenPuppet.Plugins/Manager.cs
+++ src/OpenPuppet.Plugins/Manager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static OpenPuppet.Plugins.Manager;
 
 namespace OpenPuppet.Plugins
 {
@@ -13,25 +14,45 @@ namespace OpenPuppet.Plugins
         /// or a ZIP file.
         /// </summary>
         /// <param name="path">The path to directory/ZIP file</param>
-        public static void InstallPlugin(string path)
+        /*public static InstallPluginResult InstallPlugin(string path)
         {
-            if (Directory.Exists(path)) InstallPluginDir(path);
-            else InstallPluginArchive(path);
+            if (Directory.Exists(path)) return InstallPluginDir(path);
+            else return InstallPluginArchive(path);
         }
 
-        public static async Task InstallPluginAsync(string path)
+        public static async Task<InstallPluginResult> InstallPluginAsync(string path)
         {
-            return;
+            if (Directory.Exists(path)) return await InstallPluginDirAsync(path);
+            else return await InstallPluginArchiveAsync(path);
         }
 
-        static internal void InstallPluginDir(string path)
+        static internal InstallPluginResult InstallPluginDir(string path)
         {
             
         }
 
-        static internal void InstallPluginArchive(string path)
+        static internal InstallPluginResult InstallPluginArchive(string path)
         {
 
         }
+
+        static internal async Task<InstallPluginResult> InstallPluginDirAsync(string path)
+        {
+
+        }
+
+        static internal async Task<InstallPluginResult> InstallPluginArchiveAsync(string path)
+        {
+
+        }
+
+        public class InstallPluginResult
+        {
+            public int InstalledPlugins;
+            public int FailedPlugins;
+            public int TotalPlugins;
+
+            public string[] Logs;
+        }*/
     }
 }
