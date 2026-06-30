b1a9a92093b2e64386d6346e043e6e276b03b72d
William Dawson <William@wtdawson.info>
2026-06-30 23:00:03 +0100

    Moved OpenPuppet.Plugins, and started to add the core plugins

---
+++ src/Plugins/OpenPuppet.Core.SVG/Plugin.cs
@@ -0,0 +1,19 @@
+﻿using OpenPuppet.DataTypes.Plugin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenPuppet.Core.SVG
+{
+    public class Plugin : IPlugin
+    {
+        public string PluginID { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void OnInitialized()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
