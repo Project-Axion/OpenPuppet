6b827224929d1902c0973d02a39317fdea9b71bc
Samma <81291107+Samma2009@users.noreply.github.com>
2026-07-01 15:36:20 +0200

    fix confilicts

--- src/Plugins/OpenPuppet.Core/Program.cs
+++ src/Plugins/OpenPuppet.Core/Program.cs
@@ -15,6 +15,7 @@ namespace OpenPuppet.Core
             IUIWindow.Register("openpuppet.core.timeline", typeof(Timeline));
 
             ContexMenu.AddMenuItem("View.Timeline", () => IUIWindow.Open("openpuppet.core.timeline"));
+
             Logger.WriteLine(SDK.Logger.ILogger.Level.Log, "Hello Core");
         }
     }
