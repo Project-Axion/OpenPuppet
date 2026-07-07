dfff919a9a26b3d7d5aad2ff86e689d41a058bac
William Dawson <William@wtdawson.info>
2026-07-07 22:18:52 +0100

    Mesh manager

---
+++ src/OpenPuppet.DataTypes/Projects/MeshManager.cs
@@ -0,0 +1,41 @@
+﻿using OpenPuppet.rendering;
+using OpenPuppet.rendering.VertexTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenPuppet.SDK.Projects
+{
+    public static class MeshManager
+    {
+        public static Dictionary<string, object> RegisteredMeshes { get; } = new();
+
+        public static void Register(string registry, object model)
+        {
+            RegisteredMeshes.Add(registry, model);
+        }
+
+        public static VertexMesh<T> Fetch<T>(string registry) where T : IVertex<T>
+        {
+            if (RegisteredMeshes.TryGetValue(registry, out var mesh))
+            {
+                if(mesh is VertexMesh<T> typedMesh)
+                {
+                    return typedMesh;
+                } else
+                {
+                    throw new ArgumentException($"Mesh registered under the registry '{registry}' is not {typeof(T).Name}.");
+                }
+            }
+            else throw new ArgumentException($"No mesh registered under the registry: '{registry}'.");
+        }
+
+        public static void Remove(string register)
+        {
+            if (RegisteredMeshes.ContainsKey(register))
+                RegisteredMeshes.Remove(register);
+        }
+    }
+}
---
+++ src/OpenPuppet.DataTypes/Projects/SceneManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenPuppet.SDK.Projects
+{
+    public static class SceneManager
+    {
+        public static Dictionary<string, object> RegisteredScenes { get; } = new();
+        public static object? ActiveScene { get; internal set; }
+    }
+}
